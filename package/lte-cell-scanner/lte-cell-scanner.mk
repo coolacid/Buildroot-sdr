8bb96a73711dda6f4afad4d4e42e9f16d4d8421c
CoolAcid <coolacid@gmail.com>
2018-05-18 21:50:26 -0400

    Add BOOST deps

--- package/lte-cell-scanner/lte-cell-scanner.mk
+++ package/lte-cell-scanner/lte-cell-scanner.mk
@@ -6,7 +6,7 @@
 
 LTE_CELL_SCANNER_VERSION = ef1ad25626dcf313209f07b7bebd54e1b8523cea
 LTE_CELL_SCANNER_SITE = git://github.com/JiaoXianjun/LTE-Cell-Scanner.git
-LTE_CELL_SCANNER_DEPENDENCIES =  librtlsdr
+LTE_CELL_SCANNER_DEPENDENCIES =  librtlsdr boost
 
 $(eval $(cmake-package))
 
