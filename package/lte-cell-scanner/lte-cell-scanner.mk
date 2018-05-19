96a7275abf9c096dc6b2e287d7aa24619eb2a45b
CoolAcid <coolacid@gmail.com>
2018-05-18 21:01:27 -0400

    Start LTE Cell Scanner

---
+++ package/lte-cell-scanner/lte-cell-scanner.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# LTE_CELL_SCANNER
+#
+################################################################################
+
+LTE_CELL_SCANNER_VERSION = ef1ad25626dcf313209f07b7bebd54e1b8523cea
+LTE_CELL_SCANNER_SITE = git://github.com/JiaoXianjun/LTE-Cell-Scanner.git
+LTE_CELL_SCANNER_DEPENDENCIES =  librtlsdr
+
+$(eval $(cmake-package))
+
